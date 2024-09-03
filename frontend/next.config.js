83f2058fd441fdbaafe49a437963e5499d615827
yang <oseau@me.com>
2024-09-03 19:22:23 +0800

    Revert "build: Generate static front-end build output"
    This reverts commit f7f48411921d2c25512a318c9c91052b282ccd21.

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,10 +4,6 @@ const { i18n } = require('./next-i18next.config.js')
 const nextConfig = {
   reactStrictMode: true,
   i18n,
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
 }
 
 module.exports = nextConfig
