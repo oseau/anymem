ec6741b8a753a1abeecb6575b29109f66adaa2ca
yang <oseau@me.com>
2024-09-03 15:51:34 +0800

    fix: update Tailwind CSS configuration

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,9 +1,8 @@
-import 'tailwindcss/tailwind.css'
-import type { AppProps } from 'next/app'
-import React from 'react'
+import type { AppProps } from "next/app";
+import React from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
