12923a7f5fa1645bab32542536a0f24aa96e7ba8
yang <oseau@me.com>
2024-09-03 18:10:12 +0800

    manually

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from 'next'
+
+const nextConfig: NextConfig = {
   reactStrictMode: true,
   swcMinify: true,
 }
 
-module.exports = nextConfig
+export default nextConfig
