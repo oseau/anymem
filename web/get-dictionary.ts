1278f2141542a6351ddfdc8ee690b0563521b434
yang <oseau@me.com>
2024-09-23 17:50:29 +0800

    Use inferred type Dictionary

--- web/get-dictionary.ts
+++ web/get-dictionary.ts
@@ -1,100 +1,5 @@
 import type { Locale } from "./i18n-config";
 
-// Update this interface to include the new "auth" property
-interface Dictionary {
-  title: string;
-  description: string;
-  header: { features: string; comingSoon: string };
-  hero: {
-    title: string;
-    description1: string;
-    description2: string;
-    startLearning: string;
-    watchDemo: string;
-  };
-  features: {
-    learnMore: string;
-    spacedRepetition: {
-      title: string;
-      description: string;
-      whatIs: {
-        title: string;
-        description: string;
-      };
-      algorithm: {
-        title: string;
-        description: string;
-        features: string[];
-      };
-      howItWorks: {
-        title: string;
-        steps: string[];
-      };
-      retentionCurve: {
-        title: string;
-        imageAlt: string;
-      };
-      benefits: {
-        title: string;
-        list: string[];
-      };
-      chartExplanation: {
-        title: string;
-        description: string;
-        optimizedReviews: string;
-        avoidEarlyReviews: string;
-        maximizePerformance: string;
-      };
-    };
-    multiPlatform: {
-      title: string;
-      description: string;
-      mobile: {
-        title: string;
-        description: string;
-      };
-      desktop: {
-        title: string;
-        description: string;
-      };
-      paper: {
-        title: string;
-        description: string;
-      };
-      syncFeature: {
-        title: string;
-        description: string;
-      };
-    };
-    customizableDecks: {
-      title: string;
-      description: string;
-      benefits: {
-        title: string;
-        list: string[];
-      };
-      howTo: {
-        title: string;
-        steps: string[];
-      };
-      getStarted: {
-        title: string;
-        description: string;
-        buttonText: string;
-      };
-    };
-  };
-  footer: {
-    copyright: string;
-  };
-  auth: {
-    signIn: string;
-    signUp: string;
-  };
-}
-
-// We enumerate all dictionaries here for better linting and typescript support
-// We also get the default import for cleaner types
 const dictionaries = {
   en: () => import("./dictionaries/en.json").then((module) => module.default),
   es: () => import("./dictionaries/es.json").then((module) => module.default),
@@ -106,6 +11,8 @@ const dictionaries = {
   "zh-TW": () =>
     import("./dictionaries/zh-TW.json").then((module) => module.default),
 };
+
+type Dictionary = Awaited<ReturnType<typeof dictionaries["en"]>>;
 
 export const getDictionary = async (locale: Locale): Promise<Dictionary> => {
   return dictionaries[locale]?.() ?? dictionaries.en();
