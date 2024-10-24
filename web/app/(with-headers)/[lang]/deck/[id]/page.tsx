1c9da48eb2cfd347dac281eb13503216215cea8b
yang <oseau@me.com>
2024-10-24 20:19:57 +0800

    founder mode

--- web/app/(with-headers)/[lang]/deck/[id]/page.tsx
+++ web/app/(with-headers)/[lang]/deck/[id]/page.tsx
@@ -1,8 +1,8 @@
 import { getDictionary } from "@/get-dictionary";
+import Link from "next/link";
 import { CardList } from "@/components/CardList";
 import { Locale, i18n } from "@/i18n-config";
 import { getUserDeckCards } from "@/app/actions/decks";
-import { notFound } from "next/navigation";
 
 export async function generateStaticParams() {
   return i18n.locales.map((locale) => ({ lang: locale }));
@@ -16,7 +16,20 @@ export default async function UserDeckPage({
   const dict = await getDictionary(lang);
   const deck = await getUserDeckCards(id);
   if (!deck) {
-    notFound();
+    return (
+      <div className="flex flex-col items-center justify-center gap-5">
+        <h2 className="text-2xl font-bold">Not Found</h2>
+        <p className="text-sm text-gray-500">
+          Could not find requested resource
+        </p>
+        <Link
+          href="/"
+          className="inline-block bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg shadow-lg transition duration-300"
+        >
+          Return Home
+        </Link>
+      </div>
+    );
   }
   return <CardList deck={deck} dict={dict} />;
 }
